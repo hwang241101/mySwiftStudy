36de457fdd32c23fab5471f48fd4ac4fd9255fd2
hwang241101 <hwang241101@gmail.com>
2025-09-16 09:44:58 +0900

    Initial Commit

---
+++ mySwiftStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mySwiftStudy
+//
+//  Created by yong on 2025/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mySwiftStudy/mySwiftStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySwiftStudyApp.swift
+//  mySwiftStudy
+//
+//  Created by yong on 2025/09/16.
+//
+
+import SwiftUI
+
+@main
+struct mySwiftStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
